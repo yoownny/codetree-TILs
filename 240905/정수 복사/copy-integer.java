58bdeecda5a55a06eba723247d1a871390fd70e2
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-05 18:54:32 +0900

    Title: 정수 복사, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240905/정수 복사/copy-integer.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        b = a;
+        int result = a*b;
+        System.out.println(a + " " + b);
+        System.out.println(result);
+    }
+}
