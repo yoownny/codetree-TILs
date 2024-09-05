0f14ce4421ba5a5e4251c898e18a93a2fa7018a5
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-05 18:41:16 +0900

    Title: 변수 출력하기 3, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240905/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+        System.out.printf("%d->%d->%c",a,b,c);
+    }
+}
