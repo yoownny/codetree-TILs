534b4965243cd9d7416bf88c0fdb128dd50c2457
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-05 18:34:05 +0900

    Title: 변수 선언하기, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240905/변수 선언하기/declaring-variables.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        char b = 'C';
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
