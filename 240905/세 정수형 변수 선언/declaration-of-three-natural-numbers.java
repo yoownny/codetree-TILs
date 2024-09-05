0b592d8f18fab3a2490bad9b2962e22ef22490c9
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-05 18:38:14 +0900

    Title: 세 정수형 변수 선언, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240905/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 23;
+        int c = 30;
+        System.out.printf("%d + %d = %d",a,b,c);
+    }
+}
