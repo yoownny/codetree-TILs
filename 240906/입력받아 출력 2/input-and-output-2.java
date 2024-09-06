9fc863438d1391682775753882f0c32dfcb24d57
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-06 16:07:38 +0900

    Title: 입력받아 출력 2, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240906/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int a = sc.nextInt();
+        int b= sc.nextInt();
+        System.out.print(a);
+        System.out.print(b);
+    }
+}
