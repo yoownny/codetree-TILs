b144acac4fd3e76601eae865a658105ebcb80e1e
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-22 22:52:01 +0900

    Title: 시력 검사 2, Time: 129ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240922/시력 검사 2/eye-test-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        if(a >= 1.0)
+            System.out.println("High");
+        else if(a >= 0.5)
+            System.out.println("Middle");
+        else
+            System.out.println("Low");
+    }
+}
