c3e87d1ff76d45f144df12ed02809cebe948ab11
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-22 23:42:47 +0900

    Title: 두 번의 연산, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240922/두 번의 연산/two-operations.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a % 2 == 1)
+            a += 3;
+        if(a % 3 == 0)
+            a /= 3;
+        System.out.println(a);
+    }
+}
