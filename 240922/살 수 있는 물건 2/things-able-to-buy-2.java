c68810fd8c7b0167079691ce90cf34e55343c9ec
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-22 23:11:08 +0900

    Title: 살 수 있는 물건 2, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240922/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n >= 3000)
+            System.out.println("book");
+        else if(n >= 1000)
+            System.out.println("mask");
+        else if(n >= 500)
+            System.out.println("pen");
+        else
+            System.out.println("no");
+    }
+}
