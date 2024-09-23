60574f26fe972ad2cbad0fd7dca5316403a2bd40
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-23 13:31:10 +0900

    Title: 비교 연산, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240923/비교 연산/comparison-operator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>=b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        if(a>b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        if(a<=b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        if(a<b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        if(a==b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        if(a!=b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
