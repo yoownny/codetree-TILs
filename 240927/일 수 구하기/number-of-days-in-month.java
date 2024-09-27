bec101bb759ab47767909d4ab102cfebce444e65
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-27 23:39:51 +0900

    Title: 일 수 구하기, Time: 218ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240927/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==2)
+            System.out.println(28);
+        else if ((n==1)||(n==3)||(n==5)||(n==7)||(n==8)||(n==10)||(n==12))
+            System.out.println(31);
+        else
+            System.out.println(30);
+    }
+}
