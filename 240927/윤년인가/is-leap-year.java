c710a1d41e327abba492cf442b3e821f2e95e385
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-09-27 23:36:37 +0900

    Title: 윤년인가, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 240927/윤년인가/is-leap-year.java
+++ 240927/윤년인가/is-leap-year.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
         if(y%4==0){
-            if(y%400!=0)
+            if((y%100==0)&&(y%400!=0))
                 System.out.println("false");
             else
                 System.out.println("true");
