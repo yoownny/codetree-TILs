3d8ea098e838ab809d0141e5287ac036fa6ffc91
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-10-03 16:56:42 +0900

    Title: 가장 짧은 문자열, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241003/가장 짧은 문자열/shortest-string.java
+++ 241003/가장 짧은 문자열/shortest-string.java
@@ -3,26 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String a = sc.next();
         String b = sc.next();
         String c = sc.next();
+
         int lena = a.length();
         int lenb = b.length();
         int lenc = c.length();
+
         int min = 20;
         int max = 0;
+
         if(lena<min)
             min = lena;
-        else if(lenb<min)
+        if(lenb<min)
             min = lenb;
-        else if (lenc<min)
+        if (lenc<min)
             min = lenc;
+
         if(lena>max)
             max = lena;
-        else if (lenb>max)
+        if (lenb>max)
             max = lenb;
-        else if (lenc>max)
+        if (lenc>max)
             max = lenc;
+            
         System.out.println(max-min);
     }
 }
