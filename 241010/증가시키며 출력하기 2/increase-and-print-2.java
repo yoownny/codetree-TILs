6ca820a701c8963e30e59a2af76db8a7af99a2e3
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-10-10 13:38:13 +0900

    Title: 증가시키며 출력하기 2, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241010/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        while(i<=26){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
