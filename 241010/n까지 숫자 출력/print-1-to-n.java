285bebac6901f36888861504d44b06f4aed83951
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-10-10 13:40:11 +0900

    Title: n까지 숫자 출력, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241010/n까지 숫자 출력/print-1-to-n.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        while(i<=n){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
