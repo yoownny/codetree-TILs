cb4113e33d98cc076ff3d2567d35c5f1fa2d16e4
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-10-28 23:39:53 +0900

    Title: 감소시키며 출력하기 2, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241028/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 26;
+        while(i >= 10){
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
