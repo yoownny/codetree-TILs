842d1af6308114a21450af1ae28640e029b185c1
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-10-29 12:26:41 +0900

    Title: 2 또는 3의 배수, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241029/2 또는 3의 배수/multiple-of-2-or-3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1 ; i <= n ; i++){
+            if(i%2==0 || i%3==0)
+                System.out.print(1 + " ");
+            else
+                System.out.print(0 + " ");
+        }
+    }
+}
