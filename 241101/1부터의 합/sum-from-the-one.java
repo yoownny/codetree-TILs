94a4d3e73e5654789b99a88b1532a01f08faa343
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-11-01 02:11:30 +0900

    Title: 1부터의 합, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 241101/1부터의 합/sum-from-the-one.java
+++ 241101/1부터의 합/sum-from-the-one.java
@@ -5,11 +5,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int sum = 0;
+        int result = 1;
         for(int i = 1 ; i <= 100 ; i++ ){
             if(sum >= n)
                 break;
+            result = i;
             sum += i;
         }
-        System.out.println(i);
+        System.out.println(result);
     }
 }
