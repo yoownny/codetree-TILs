5bd45cef073f5c7a48478de15bf4b4dd05af3df5
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-11-01 02:07:50 +0900

    Title: 친근하지 않은 수, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241101/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        for(int i = 1 ; i <= n ; i++ ){
+            if(i % 2 == 0)
+                continue;
+            if(i % 3 == 0)
+                continue;
+            if(i % 5 == 0)
+                continue;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
