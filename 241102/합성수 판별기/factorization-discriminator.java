690cc0a0e29cae5fb0d95eeaf52e7b880e36807e
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-11-02 02:19:50 +0900

    Title: 합성수 판별기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 241102/합성수 판별기/factorization-discriminator.java
+++ 241102/합성수 판별기/factorization-discriminator.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner;
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        char result = 'N';
+        String result = "N";
         for(int i = 2 ; i <= n-1 ; i++) {
             if(n % i == 0){
-                result = 'Y';
+                result = "C";
                 break;
             }
         }
