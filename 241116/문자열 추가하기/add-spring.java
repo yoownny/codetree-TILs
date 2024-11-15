636fa338f44fe86822e82e7dcd671c73d4a94046
Seungyooon <139729585+yoownny@users.noreply.github.com>
2024-11-16 04:33:37 +0900

    Title: 문자열 추가하기, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

--- 241116/문자열 추가하기/add-spring.java
+++ 241116/문자열 추가하기/add-spring.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
-        input + "Hello";
+        input += "Hello";
         System.out.println(input);
     }
 }
