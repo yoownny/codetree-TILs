7db12c30142c3f5304f31dac101699c8759c5e3c
Seungyooon <139729585+yoownny@users.noreply.github.com>
2025-01-18 15:46:13 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 130ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250118/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int isFind(int n, int[] num){
+        if(n == 0){
+            return num[0];
+        }
+
+        return Math.max(isFind(n-1,num),num[n]);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] num = new int[n];
+        for(int i = 0 ; i < n ; i++ ){
+            num[i] = sc.nextInt();
+        }
+        int max = num[0];
+        System.out.println(isFind(n, num));
+    }
+}
