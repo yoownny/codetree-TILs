632265250f56453ca1b55d3497474f22357a7460
Seungyooon <139729585+yoownny@users.noreply.github.com>
2025-01-28 15:46:10 +0900

    Title: 만나는 그 순간, Time: 128ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250128/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] A = new int[1000];
+        int[] B = new int[1000];
+        int cnt1 = 0;
+        int station = 0;
+        int cnt2 = 0;
+        int station2 = 0;
+
+        for(int i = 0 ; i < N ; i++ ){
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+            if(d == 'L'){
+                while(t > 0){
+                    station--;
+                    A[cnt1] = station;
+                    cnt1++;
+                    t--;
+                }
+            }else if(d == 'R'){
+                while(t > 0){
+                    station++;
+                    A[cnt1] = station;
+                    cnt1++;
+                    t--;
+                }
+            }
+        }
+
+        for(int i = 0 ; i < M ; i++ ){
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+            if(d == 'L'){
+                while(t > 0){
+                    station2--;
+                    B[cnt2] = station2;
+                    cnt2++;
+                    t--;
+                }
+            }else if(d == 'R'){
+                while(t > 0){
+                    station2++;
+                    B[cnt2] = station2;
+                    cnt2++;
+                    t--;
+                }
+            }
+        }
+
+        int flag = 0;
+        for(int i = 0 ; i < cnt1 ; i++){
+            if(A[i] == B[i]){
+                System.out.println(i+1);
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0){
+            System.out.println(-1);
+        }
+    }
+}
