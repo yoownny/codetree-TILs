15fd613392f42afb20dfcfe5214abf24babc4cd7
Seungyooon <139729585+yoownny@users.noreply.github.com>
2025-02-17 00:42:26 +0900

    Title: 작은 구슬의 이동, Time: 94ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250217/작은 구슬의 이동/small-marble-movement.java
+++ 250217/작은 구슬의 이동/small-marble-movement.java
@@ -4,8 +4,9 @@ import java.util.*;
 public class Main {
 	static int N;
 
+	// 격자 범위 확인 함수
 	public static boolean isRange(int nx, int ny) {
-		return nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= N - 1;
+		return nx >= 0 && nx < N && ny >= 0 && ny < N;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -14,24 +15,23 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		int T = Integer.parseInt(st.nextToken());
 
-		int[] dx = new int[] { 1, 0, -1, 0 };
-		int[] dy = new int[] { 0, -1, 0, 1 };
+		// 방향 배열 수정 (U, D, R, L 순서)
+		int[] dx = { 0, 0, 1, -1 };  // {U, D, R, L}
+		int[] dy = { -1, 1, 0, 0 };  // {U, D, R, L}
 
 		st = new StringTokenizer(br.readLine(), " ");
 		int R = Integer.parseInt(st.nextToken());
 		int C = Integer.parseInt(st.nextToken());
 		char D = st.nextToken().charAt(0);
 
+		// 방향 매핑 수정
 		int d = 0;
-		if (D == 'R')
-			d = 0;
-		else if (D == 'D')
-			d = 1;
-		else if (D == 'L')
-			d = 2;
-		else if (D == 'U')
-			d = 3;
+		if (D == 'U') d = 0;
+		else if (D == 'D') d = 1;
+		else if (D == 'R') d = 2;
+		else if (D == 'L') d = 3;
 
+		// (1-based) 입력값을 (0-based)로 변환
 		int y = R - 1;
 		int x = C - 1;
 
@@ -43,14 +43,12 @@ public class Main {
 				x = nx;
 				y = ny;
 			} else {
-				d = (d + 2) % 4;
+				d = (d + 2) % 4; // 방향 반대로 변경
 			}
 			T--;
 		}
 
-		x++;
-		y++;
-		System.out.println(y + " " + x);
-
+		// (0-based) 좌표를 (1-based)로 변환하여 출력
+		System.out.println((y + 1) + " " + (x + 1));
 	}
 }
