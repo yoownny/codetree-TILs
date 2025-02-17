ca28b9ba8521070ecfc7453f75da3c91975e1f3d
Seungyooon <139729585+yoownny@users.noreply.github.com>
2025-02-18 00:02:46 +0900

    Title: N개 중에 M개 뽑기, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250218/N개 중에 M개 뽑기/n-choose-m.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[] result;
+	static boolean[] visited;
+	static int K;
+	static int N;
+	static StringBuffer sb = new StringBuffer();
+
+	public static void isPrint(int num) {
+		if (num == N) {
+			for (int i = 0; i < N; i++) {
+				sb.append(result[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= K; i++) {
+			if (!visited[i]) {
+				if (num == 0 || result[num - 1] < i) {
+					visited[i] = true;
+					result[num] = i;
+					isPrint(num + 1);
+					visited[i] = false;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+
+		result = new int[N];
+		visited = new boolean[K + 1];
+
+		isPrint(0);
+		System.out.println(sb);
+
+	}
+}
